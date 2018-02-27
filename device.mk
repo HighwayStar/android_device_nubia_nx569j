86ddd249f8f045b12a6270b77a639718ba9ef7a1
Vitaliy Tomin <highwaystar.ru@gmail.com>
2018-02-27 21:10:18 +0800

    nx569j: add keystore package

--- device.mk
+++ device.mk
@@ -116,6 +116,10 @@ PRODUCT_PACKAGES += \
     sap.conf \
     xtwifi.conf
 
+# Keystore
+PRODUCT_PACKAGES += \
+    keystore.msm8952
+
 # IRSC
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:system/etc/sec_config

98621d062cce15b6cc1abe5458f0b51d27fec799
Vitaliy Tomin <highwaystar.ru@gmail.com>
2018-03-05 22:52:31 +0800

    nx569j: add power.msm8952 package and board flags

--- device.mk
+++ device.mk
@@ -253,6 +253,11 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.vulkan.level-0.xml:system/etc/permissions/android.hardware.vulkan.level.xml \
     frameworks/native/data/etc/android.hardware.vulkan.version-1_0_3.xml:system/etc/permissions/android.hardware.vulkan.version.xml
 
+
+# Power
+PRODUCT_PACKAGES += \
+    power.msm8952
+
 # Ramdisk
 PRODUCT_PACKAGES += \
     fstab.qcom \

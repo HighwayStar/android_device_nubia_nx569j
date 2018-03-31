c893f23844034a2b8fa55e341a1a96dee94d6991
Vitaliy Tomin <highwaystar.ru@gmail.com>
2018-03-31 19:06:41 +0800

    nx569j: Enable oss camera building, drop shims

--- device.mk
+++ device.mk
@@ -90,14 +90,11 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     camera.device@3.2-impl \
+    camera.msm8952 \
+    libmm-qcamera \
     Snap \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor
-
-# Camera libshims
-PRODUCT_PACKAGES += \
-    libshim_camera \
-    libshims_cameraservice
 
 # Configstore
 PRODUCT_PACKAGES += \

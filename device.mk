e65de32215f2b265f11adb060744df3283627f26
Vitaliy Tomin <highwaystar.ru@gmail.com>
2018-05-10 00:21:08 +0800

    nx569j: Enable livedisplay

--- device.mk
+++ device.mk
@@ -227,6 +227,10 @@ PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service \
     lights.msm8952
 
+# LiveDisplay native
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@1.0-service-sdm
+
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \

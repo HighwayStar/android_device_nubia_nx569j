02523ec03cda5aaaf257daaf3bf4a361678c8052
Nich <nctrenco@gmail.com>
2018-10-21 17:22:45 +0800

    nx569j: biometrics: Add sanity check for enumerate() return value
    On successful enumeration, the max fingerprint pointer would have been
    modified to match the total number of fingerprints. The total number
    would also be returned directly. This is in accordance with the 2.0
    enumerate() behaviour, as seen in the old libhardware header:
    
        /*
         * Enumerate all the fingerprint templates found in the directory set by
         * set_active_group()
         ...
         * - The size of the array provided, in fingerprint_finger_id_t elements.
         * Max_size is a bi-directional parameter and returns the actual number
         * of elements copied to the caller supplied array.
         * In the absence of errors the function returns the total number of templates
         ...
         * The caller of this function has a complete list of the templates when *max_size
         * is the same as the function return.
         *
         * Function return: Total number of fingerprint templates in the current storage directory.
         *                  or a negative number in case of error, generally from the errno.h set.
         */
        int (*enumerate)(struct fingerprint_device *dev, fingerprint_finger_id_t *results,
            uint32_t *max_size);
    
    For the case of Goodix, enumerate() is a dummy function that does
    nothing. 0 would be returned and n would be untouched. However our
    previous check for (ret == 0) was always true for Goodix even with
    stored fingerprints, triggering the framework for a complete cleanup
    for all fingerprints and deleting saved fingerprints every reboot (or
    to be exact, each time the HAL (re)starts).
    
    [BACKPORT FROM P] This change is redundant on O, since fingerprint
    cleanup was only enabled in P. But this fixes the enumeration logic
    anyway.
    
    Change-Id: Ibfdbce407fb04448bf740a06dac0d015fc433df2

--- biometrics/BiometricsFingerprint.cpp
+++ biometrics/BiometricsFingerprint.cpp
@@ -188,8 +188,36 @@ Return<RequestStatus> BiometricsFingerprint::cancel() {
     return ErrorFilter(ret);
 }
 
+
+#define MAX_FINGERPRINTS 100
+
+typedef int (*enumerate_2_0)(struct fingerprint_device *dev, fingerprint_finger_id_t *results,
+uint32_t *max_size);
+
 Return<RequestStatus> BiometricsFingerprint::enumerate()  {
-	    return ErrorFilter(mDevice->enumerate(mDevice));
+    fingerprint_finger_id_t results[MAX_FINGERPRINTS];
+    uint32_t n = MAX_FINGERPRINTS;
+    enumerate_2_0 enumerate = (enumerate_2_0) mDevice->enumerate;
+    int total_templates = enumerate(mDevice, results, &n);
+
+    ALOGD("Got %d enumerated templates, retval = %d", n, total_templates);
+
+    // Check if the function actually enumerated, or just simply sent a dummy retval.
+    if ((n == MAX_FINGERPRINTS && total_templates < MAX_FINGERPRINTS)
+            || mClientCallback == nullptr) {
+        return RequestStatus::SYS_EINVAL;
+    }
+
+    for (uint32_t i = 0; i < n; i++) {
+        const uint64_t devId = reinterpret_cast<uint64_t>(mDevice);
+        const auto& fp = results[i];
+        ALOGD("onEnumerate(fid=%d, gid=%d)", fp.fid, fp.gid);
+        if (!mClientCallback->onEnumerate(devId, fp.fid, fp.gid, n - i - 1).isOk()) {
+            ALOGE("failed to invoke fingerprint onEnumerate callback");
+        }
+    }
+
+    return RequestStatus::SYS_OK;
 }
 
 Return<RequestStatus> BiometricsFingerprint::remove(uint32_t gid, uint32_t fid) {

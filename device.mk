a46992a94623e34325008dd0a52eec7326c47ff4
Vitaliy Tomin <highwaystar.ru@gmail.com>
2019-05-11 20:55:28 +0800

    nx569j: Remove livedisplay hidl
    * none of curerent lineage livedisplay hdils is compatible with
    nubia blobs. legacycmm missing calibration files for mm-daemon-pp,
    sysfs variant missing sysfs nodes in kernel

--- device.mk
+++ device.mk
@@ -219,11 +219,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.nx569j
 
-# LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm
-
-
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
